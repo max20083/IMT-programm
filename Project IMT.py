6006414ab2ff7d129511a2c5f0261969280c630f
Maxim <maxmixeev2008@gmail.com>
2025-09-17 19:06:27 +0300

    Update Project IMT.py
    added a value check and added a button to clear the data file.

--- Project IMT.py
+++ Project IMT.py
@@ -3,18 +3,30 @@ from tkinter import *
 def clicked():
     rost_get = rost_input.get()
     mas_get = mas_input.get()
-    r = (float(rost_get))/100
-    m = (float(mas_get))
-    imt = round(m / (r ** 2), 2)
-    otv.configure(text=(f'Индекс - '+ str(imt)))
-    try:
-        with open('Резулльтаты индекса масс тела.txt', 'a', encoding='utf-8') as file:
-            file.write('Индекс массы тела при росте ' + str(r) + '(м) и весе' + str(m) + '(кг) равен ' + (str(imt)) + '\n')
-    except:
+    if rost_get!= float or mas_get != float:
+        otv.configure(text='Введите в поле ввода число)')
+    else:
+     r = (float(rost_get))/100
+     m = (float(mas_get))
+     imt = round(m / (r ** 2), 2)
+     otv.configure(text=(f'Индекс - '+ str(imt)))
+     try:
+        with open('Резулльтаты индекса масс тела', 'a', encoding='utf-8') as file:
+            file.write('Индекс массы тела при росте ' + str(r) + '(м) и весе ' + str(m) + '(кг) равен ' + (str(imt)) + '\n')
+            file.close()
+     except:
         otv.configure(text='Ошибка при работе с файлом')
+def clear_one():
+    try:
+        with open('Резулльтаты индекса масс тела', 'w', encoding='utf-8') as file:
+            file.write(' ')
+            file.close()
+    except:
+        otv.configure(text='ошибка при работе с файлом')
+
 root = Tk()
 root.title('Расчет индекча массы тела')
-root.geometry('400x200')
+root.geometry('500x200')
 lbl=Label(root , text='Привет это приложение длдя расчета индекса массы тела')
 lbl.grid(column=0, row=0)
 rost=Label(root , text='Введи сюда свой рост  ')
@@ -23,6 +35,7 @@ mas=Label(root , text='А сюда свой вес  ')
 mas_input= Entry(root,width=10)
 rasch=Button(root , text='Расчитать индекс массы тела', command = clicked)
 otv=Label(root , text='индекс - ?')
+clear_Button=Button(root, text='Clear data', command=clear_one, width=8, height=8)
 
 
 lbl.pack()
@@ -32,4 +45,6 @@ mas.pack()
 mas_input.pack()
 rasch.pack(pady=10)
 otv.pack(pady=10)
+clear_Button.pack()
+clear_Button.place(x=400, y=35)
 root.mainloop()
